5dc22621213e97a9dae3869f4c27faf49a7b0fa9
logicfirst <slee@logicfirstllc.com>
2020-11-02 14:36:46 -0500

    commit tree dded137b121d8c08022054a4d19f4c021b1c506b

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,17 +1,17 @@
 def create_an_empty_array
-  empty_array = []
+  []
 end
 
 def create_an_array
-  an_array = ["element1", "element2", "element3", "element4"]
+  [1,2,3,4]
 end
 
 def add_element_to_end_of_array(array, element)
-  array << element
+  array << element 
 end
 
 def add_element_to_start_of_array(array, element)
-  array.unshift(element)
+  array.unshift element
 end
 
 def remove_element_from_end_of_array(array)
@@ -36,5 +36,4 @@ end
 
 def update_element_from_index(array, index_number, element)
   array[index_number] = element
-
 end
